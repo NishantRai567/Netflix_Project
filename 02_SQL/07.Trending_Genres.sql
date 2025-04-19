3414ed738103fcd8c9d81683f09eab7aa2ec22f7
Nishant Rai <nishant_rai123@hotmail.com>
2025-04-19 17:29:11 +0100

    Changed sql files

--- 02_SQL/07.Trending_Genres.sql
+++ 02_SQL/07.Trending_Genres.sql
@@ -1,15 +1,27 @@
 -- What genres are trending over time
 
+WITH genre_counts as(
 SELECT
-    EXTRACT(YEAR FROM clean_release_date) AS year,
-    "Type" AS genre,
-    COUNT(*) AS title_count
+    EXTRACT(YEAR FROM clean_release_date) as year,
+    "Type" as genre,
+    COUNT(*) as total_titles
 FROM netflix_cleaned
 WHERE EXTRACT(YEAR FROM clean_release_date)>2000
 GROUP BY 
     EXTRACT(YEAR FROM clean_release_date),
-    "Type"
-ORDER BY
-    EXTRACT(YEAR FROM clean_release_date),
-    title_count DESC;
-
+    "Type"),
+ranked_genres as(
+SELECT
+    year,
+    genre,
+    total_titles,
+    rank() over (partition by year order by total_titles DESC) as genre_rank
+FROM genre_counts
+)
+SELECT
+    year,
+    genre,
+    total_titles
+FROM ranked_genres
+WHERE genre_rank=1
+ORDER BY year,total_titles desc;
--- 02_SQL/08.Genres_with_Highest_Content_by_Country.sql
+++ 02_SQL/08.Genres_with_Highest_Content_by_Country.sql
@@ -1,15 +1,31 @@
 -- Which genres have the highest content volume by country
-
-SELECT 
-    "Type",
+WITH unnested_data AS (
+    SELECT
+        UNNEST(STRING_TO_ARRAY(country, ',')) AS country,
+        "Type"
+    FROM netflix_cleaned
+),
+genre_counts AS (
+    SELECT
+        country,
+        "Type" as genre,
+        COUNT(*) AS count
+    FROM unnested_data 
+    GROUP BY country, genre
+),
+ranked_genres AS (
+    SELECT
+        country,
+        genre,
+        count,
+        RANK() OVER (PARTITION BY country ORDER BY count DESC) AS rank
+    FROM genre_counts
+)
+SELECT
     country,
-    COUNT(*)
-FROM netflix_cleaned
-WHERE country is not null
-GROUP BY 
-    "Type",
-    country
-ORDER BY 
-    "Type",
-    COUNT(*) DESC;
-
+    genre
+FROM ranked_genres
+WHERE rank = 1
+ORDER BY count DESC
+LIMIT 324
+OFFSET 4;
